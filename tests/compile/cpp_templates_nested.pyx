e484bd28b8fe313c4cbf2edc14f3bd8347e5e6fa
Julianus Pfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2021-02-24 22:28:04 +0100

    test different nested style

--- tests/compile/cpp_templates_nested.pyx
+++ tests/compile/cpp_templates_nested.pyx
@@ -10,7 +10,7 @@ cdef extern from *:
     cdef cppclass Bar:
         pass
 
-cdef vector[vector[int]] a
+cdef vector[ vector[ int ] ] a
 cdef vector[vector[const int]] b
 cdef vector[vector[vector[int]]] c
 cdef vector[vector[vector[const int]]] d
